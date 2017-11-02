6521bc266ee46b1c17eaaa1b169321b91c998512
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2017-11-02 23:05:20 +0100

    added widgets

--- QtFrontend/build-untitled-Desktop_Qt_5_9_1_MSVC2015_64bit-Debug/ui_mainwindow.h
+++ QtFrontend/build-untitled-Desktop_Qt_5_9_1_MSVC2015_64bit-Debug/ui_mainwindow.h
@@ -14,8 +14,12 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -25,28 +29,50 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
+    QAction *actionEvaluate;
     QWidget *centralWidget;
+    QLineEdit *editExpression;
+    QPushButton *cmdEvaluate;
+    QLabel *txtResult;
+    QMenuBar *menuBar;
+    QMenu *menuCalculator;
+    QToolBar *mainToolBar;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
+        MainWindow->resize(414, 148);
+        actionEvaluate = new QAction(MainWindow);
+        actionEvaluate->setObjectName(QStringLiteral("actionEvaluate"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        editExpression = new QLineEdit(centralWidget);
+        editExpression->setObjectName(QStringLiteral("editExpression"));
+        editExpression->setGeometry(QRect(10, 10, 281, 20));
+        cmdEvaluate = new QPushButton(centralWidget);
+        cmdEvaluate->setObjectName(QStringLiteral("cmdEvaluate"));
+        cmdEvaluate->setGeometry(QRect(310, 10, 75, 23));
+        txtResult = new QLabel(centralWidget);
+        txtResult->setObjectName(QStringLiteral("txtResult"));
+        txtResult->setGeometry(QRect(10, 50, 161, 21));
+        MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 414, 21));
+        menuCalculator = new QMenu(menuBar);
+        menuCalculator->setObjectName(QStringLiteral("menuCalculator"));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(mainToolBar);
-        centralWidget = new QWidget(MainWindow);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        MainWindow->setCentralWidget(centralWidget);
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuCalculator->menuAction());
+        menuCalculator->addAction(actionEvaluate);
 
         retranslateUi(MainWindow);
 
@@ -56,6 +82,10 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        actionEvaluate->setText(QApplication::translate("MainWindow", "Evaluate", Q_NULLPTR));
+        cmdEvaluate->setText(QApplication::translate("MainWindow", "Evaluate", Q_NULLPTR));
+        txtResult->setText(QString());
+        menuCalculator->setTitle(QApplication::translate("MainWindow", "Calculator", Q_NULLPTR));
     } // retranslateUi
 
 };
--- QtFrontend/untitled/mainwindow.cpp
+++ QtFrontend/untitled/mainwindow.cpp
@@ -12,3 +12,8 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_cmdEvaluate_clicked()
+{
+    ui->txtResult->setText("Prdo");
+}
