6271bd96fba8447cb92f67cf55e1cced6781f55d
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2019-04-16 20:45:24 +0200

    funkcionalan CMake projekt s Catch2 testom :)

--- TestCMake/TestCMake.cpp
+++ TestCMake/TestCMake.cpp
@@ -3,14 +3,18 @@
 
 #include "TestCMake.h"
 
-#include "Function.h"
+#define CATCH_CONFIG_MAIN
 
-//#define CATCH_CONFIG_MAIN
 
+#include "../catch/catch.hpp"
+//#include "Function.h"
+
+
+/*
 int main()
 {
 	int c = func(2, 3);
 
 	std::cout << c;
 }
-
+*/
