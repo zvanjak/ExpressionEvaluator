593aeae42b8e1e61406c92f857cc578c0be01d8e
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2019-05-16 16:20:39 +0100

    more tests

--- TestCMake/tests/ErrorsTests.cpp
+++ TestCMake/tests/ErrorsTests.cpp
@@ -13,6 +13,7 @@ TEST_CASE("Test_MismatchedParenthesis", "[errors]")
 	REQUIRE(_calculator.driver(")(") == _calculator.getErrorMessage(CalculatorStatus::MISMATCHED_PARENTHESIS));
 	REQUIRE(_calculator.driver("(") == _calculator.getErrorMessage(CalculatorStatus::MISMATCHED_PARENTHESIS));
 	REQUIRE(_calculator.driver(")") == _calculator.getErrorMessage(CalculatorStatus::MISMATCHED_PARENTHESIS));
+	REQUIRE(_calculator.driver("sin(3") == _calculator.getErrorMessage(CalculatorStatus::MISMATCHED_PARENTHESIS));
 }
 
 TEST_CASE("Test_SyntaxError", "[errors]")
@@ -22,4 +23,5 @@ TEST_CASE("Test_SyntaxError", "[errors]")
 	string res = _calculator.driver("2 + + (3*5");
 
 	REQUIRE(_calculator.getErrorMessage(CalculatorStatus::SYNTAX_ERROR) == res);
+	REQUIRE(_calculator.driver("sin 3") == _calculator.getErrorMessage(CalculatorStatus::MISMATCHED_PARENTHESIS));
 }
