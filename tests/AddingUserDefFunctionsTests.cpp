6b82b605bdc1d20aaf91bfcfd1ede350f5031863
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-12-28 23:29:24 +0100

    eto

--- tests/AddingUserDefFunctionsTests.cpp
+++ tests/AddingUserDefFunctionsTests.cpp
@@ -6,7 +6,7 @@ double simple_func(double a)
 	return a * 2;
 }
 
-TEST_CASE("Test_AddSimpleFunction", "[errors]")
+TEST_CASE("Test_AddSimpleFunction1", "[errors]")
 {
 	ExpressionEvaluator _calculator;
     CalculatorStatus outStatus;
@@ -16,3 +16,22 @@ TEST_CASE("Test_AddSimpleFunction", "[errors]")
     REQUIRE(4.0 == _calculator.evaluate("simpleFunc(2)", &outStatus));    
 }
 
+TEST_CASE("Test_AddSimpleFunction2", "[errors]")
+{
+	ExpressionEvaluator _calculator;
+    CalculatorStatus outStatus;
+
+    _calculator.addUserDefinedFunction("simpleFunc", new DefinedFunctionOneParam(simple_func));
+
+    REQUIRE(8.0 == _calculator.evaluate("simpleFunc(2+2)", &outStatus));    
+}
+
+TEST_CASE("Test_AddSimpleFunction3", "[errors]")
+{
+	ExpressionEvaluator _calculator;
+    CalculatorStatus outStatus;
+
+    _calculator.addUserDefinedFunction("simpleFunc", new DefinedFunctionOneParam(simple_func));
+
+    REQUIRE(simple_func(sin(2)) == _calculator.evaluate("simpleFunc(sin(2))", &outStatus));    
+}
