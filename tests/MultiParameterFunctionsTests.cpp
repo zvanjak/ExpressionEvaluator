4420fe577f5151c8e20a747bc19af3276712d461
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-01-08 20:11:37 +0100

    wohoooo

--- tests/MultiParameterFunctionsTests.cpp
+++ tests/MultiParameterFunctionsTests.cpp
@@ -24,7 +24,7 @@ TEST_CASE("Test_SimpleTwoParamFunction2", "[errors]")
 
     _calculator.addUserDefinedFunction("simpleTwoParamFunc", new DefinedFunctionTwoParam(simpleTwoParamFunc));
 
-    //REQUIRE(simpleTwoParamFunc(2+2,2*3) == _calculator.evaluate("simpleTwoParamFunc(2+2,2*3)", &outStatus));
+    REQUIRE(simpleTwoParamFunc(2+2,2*3) == _calculator.evaluate("simpleTwoParamFunc(2+2,2*3)", &outStatus));
     REQUIRE(simpleTwoParamFunc(1+2*3,4*5-6) == _calculator.evaluate("simpleTwoParamFunc(1+2*3,4*5-6)", &outStatus));
     REQUIRE(simpleTwoParamFunc(2+2*3-4,2*3-1/8.5) == _calculator.evaluate("simpleTwoParamFunc(2+2*3-4,2*3-1/8.5)", &outStatus));
     
@@ -40,4 +40,6 @@ TEST_CASE("Test_SimpleTwoParamFunction3", "[errors]")
     _calculator.addUserDefinedFunction("simpleTwoParamFunc", new DefinedFunctionTwoParam(simpleTwoParamFunc));
 
     REQUIRE(simpleTwoParamFunc(simpleTwoParamFunc(1,1),simpleTwoParamFunc(2,2)) == _calculator.evaluate("simpleTwoParamFunc(simpleTwoParamFunc(1,1),simpleTwoParamFunc(2,2))", &outStatus));
+    REQUIRE(simpleTwoParamFunc(simpleTwoParamFunc(2+2,2*3),simpleTwoParamFunc(1+2*3,4*5-6)) == _calculator.evaluate("simpleTwoParamFunc(simpleTwoParamFunc(2+2,2*3),simpleTwoParamFunc(1+2*3,4*5-6))", &outStatus));
+    REQUIRE(simpleTwoParamFunc(simpleTwoParamFunc(-2+3,2*3),simpleTwoParamFunc(2*sin(0.5),cos(2)*3)) == _calculator.evaluate("simpleTwoParamFunc(simpleTwoParamFunc(-2+3,2*3),simpleTwoParamFunc(2*sin(0.5),cos(2)*3))", &outStatus));
 }
