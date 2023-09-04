610e5f882d97c4ba752a326c14ad89e36d9c97df
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-09-04 15:16:41 +0200

    da da

---
+++ QtFrontend/untitled/mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include "../../single_header/ExpressionEvaluator.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_cmdEvaluate_clicked()
+{
+    QString expression = ui->editExpression->text();
+
+    ExpressionEvaluator calculator;
+
+    string result = calculator.driver(expression.toStdString());
+    QString qstr = QString::fromStdString(result);
+
+    ui->txtResult->setText(qstr);
+}
+
+void MainWindow::on_editExpression_returnPressed()
+{
+    on_cmdEvaluate_clicked();
+
+    ui->editExpression->setText("");
+}
